71031d71caedd8cd3073f4e1eaac029eb66918bd
bryan erdanto <bryanputra455@gmail.com>
2023-03-24 11:01:07 +0700

    procedure input

--- insertionSort/insertionSort.cpp
+++ insertionSort/insertionSort.cpp
@@ -20,6 +20,46 @@ void inpuy() {						// Procedure Input
 	}
 	cout << endl;									// Membuat jarak per baris program
 	cout << "======================" << endl;		// Membuat tampilan susunan data element array
-	cout << "Masukkan Element Array" << endl;		
+	cout << "Masukkan Element Array" << endl;
 	cout << "======================" << endl;
+
+
+	for (int i = 0; i < n; i++)						// Menggunakan perulangan for untuk menyimpan data pada array
+	{
+		cout << "Data ke-" << (i + 1) << ": ";		// Memasukkan atau menginputkan nilai data n
+		cin >> arr[i];								// Menyimpan nilai data n kedalam array arr
+	}
+}
+
+void insertionsort() {			// Procedure Insertiondort
+
+	int temp;					// Membuat variable data temporer atau penyimpanan sementara 
+	int j;						// Membuat variable j sebagai penanda
+
+	for (int i = 1; i < n; i++) {	// 1. Looping dengan i dimulai dari 1 hingga n-1
+
+		temp = arr[i];				// 2. simpan nilai arr[i] ke variable sementara temp
+
+		j = i - 1;					// 3. setting nilai j sama dengan i-1;
+
+		while (j >= 0 && arr[j] > temp) // 4. Looping while dimana nilai j lebih besar sama dengan 0 dan 
+			// arr[j] lebih besar daripada temp
+		{
+			arr[j + 1] = arr[j];		// 4a. simpan arr[j] ke dalam variable arr[j+1]
+			j--;						// 4b. Decrement nilai j by 1
+		}
+
+		arr[j + 1] = temp;				// 5. simpan nilai temp ke dalam arr[j+1
+
+
+		cout << "\nPass " << i << ":"			// Output ke layar
+			for (int k = 0; k < n; k++) {			// Looping nilai k dimulai dari 0 hingga n-1
+				cout << arr[k] << " ";			// Output ke layar
+			}
+
+	}
+}
+
+	
+  
 
